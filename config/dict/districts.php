6a6251a23ec3e5a2cdac3ee438727e44fd197923
zernopoisk <zernopoisk@gmail.com>
2018-08-29 19:31:45 +0000

    config/dict files added (MongoDB json-version).

---
+++ config/dict/districts.php
@@ -0,0 +1,494 @@
+<?php
+
+return [
+    
+    /**
+     * Список районов.
+     */
+
+    "Vinnytsia"=>[
+        
+        "Barsky"=>[
+            "alias"=>"Barsky",
+            "name"=>"Барский",
+            "translit"=>"Barskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Барский-район.png",
+        ],
+    
+        "Bershad"=>[
+            "alias"=>"Bershad",
+            "name"=>"Бершадский",
+            "translit"=>"Bershadskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Бершадский-район.png",
+        ],
+    
+        "Vinnitsa"=>[
+            "alias"=>"Vinnitsa",
+            "name"=>"Винницкий",
+            "translit"=>"Vinnitskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Винницкий-район.png",
+        ],
+    
+        "Gaissky"=>[
+            "alias"=>"Gaissky",
+            "name"=>"Гайсинский",
+            "translit"=>"Haysynskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Гайсинский-район.png",
+        ],
+    
+        "Zhmerinsky"=>[
+            "alias"=>"Zhmerinsky",
+            "name"=>"Жмеринский",
+            "translit"=>"Zhmerinskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Жмеринский-район.png",
+        ],
+    
+        "Ilyinets"=>[
+            "alias"=>"Ilyinets",
+            "name"=>"Ильинецкий",
+            "translit"=>"Ilinetskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Ильинецкий-район.png",
+        ],
+    
+        "Kalinovsky"=>[
+            "alias"=>"Kalinovsky",
+            "name"=>"Калиновский",
+            "translit"=>"Kalinovskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Калиновский-район.png",
+        ],
+    
+        "Kazatinsky"=>[
+            "alias"=>"Kazatinsky",
+            "name"=>"Казатинский",
+            "translit"=>"Kazatynskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Казатинский-район.png",
+        ],
+    
+        "Kryzhopol"=>[
+            "alias"=>"Kryzhopol",
+            "name"=>"Крыжопольский",
+            "translit"=>"Kryzhopolskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Крыжопольский-район.png",
+        ],
+    
+        "Lipovetsky"=>[
+            "alias"=>"Lipovetsky",
+            "name"=>"Липовецкий",
+            "translit"=>"Lypovetskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Липовецкий-район.png",
+        ],
+    
+        "Litinsky"=>[
+            "alias"=>"Litinsky",
+            "name"=>"Литинский",
+            "translit"=>"Litinskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Литинский-район.png",
+        ],
+    
+        "Mogilev-Podolsky"=>[
+            "alias"=>"Mogilev-Podolsky",
+            "name"=>"Могилёв-Подольский",
+            "translit"=>"Mogilov-Podolskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Могилёв-Подольский-район.png",
+        ],
+    
+        "Murovanokurilovets"=>[
+            "alias"=>"Murovanokurilovets",
+            "name"=>"Мурованокуриловецкий",
+            "translit"=>"Murovanokurylovetskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Мурованокуриловецкий-район.png",
+        ],
+    
+        "Nemirovsky"=>[
+            "alias"=>"Nemirovsky",
+            "name"=>"Немировский",
+            "translit"=>"Nemirovskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Немировский-район.png",
+        ],
+    
+        "Oratovsky"=>[
+            "alias"=>"Oratovsky",
+            "name"=>"Оратовский",
+            "translit"=>"Oratovskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Оратовский-район.png",
+        ],
+    
+        "Peschany"=>[
+            "alias"=>"Peschany",
+            "name"=>"Песчанский",
+            "translit"=>"Peschanskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Песчанский-район.png",
+        ],
+    
+        "Pogrebischensky"=>[
+            "alias"=>"Pogrebischensky",
+            "name"=>"Погребищенский",
+            "translit"=>"Pogrebishchenskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Погребищенский-район.png",
+        ],
+    
+        "Teplicki"=>[
+            "alias"=>"Teplicki",
+            "name"=>"Тепликский",
+            "translit"=>"Teplykskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Тепликский-район.png",
+        ],
+    
+        "Tomashpol"=>[
+            "alias"=>"Tomashpol",
+            "name"=>"Томашпольский",
+            "translit"=>"Tomashpolskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Томашпольский-район.png",
+        ],
+    
+        "Trostyanets"=>[
+            "alias"=>"Trostyanets",
+            "name"=>"Тростянецкий",
+            "translit"=>"Trostyanetskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Тростянецкий-район.png",
+        ],
+    
+        "Tulchin"=>[
+            "alias"=>"Tulchin",
+            "name"=>"Тульчинский",
+            "translit"=>"Tulchynskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Тульчинский-район.png",
+        ],
+    
+        "Tyvrovsky"=>[
+            "alias"=>"Tyvrovsky",
+            "name"=>"Тывровский",
+            "translit"=>"Tyvrovskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Тывровский-район.png",
+        ],
+    
+        "Khmelnytskyi"=>[
+            "alias"=>"Khmelnytskyi",
+            "name"=>"Хмельницкий",
+            "translit"=>"Khmelnitskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Хмельницкий-район.png",
+        ],
+    
+        "Chernevets"=>[
+            "alias"=>"Chernevets",
+            "name"=>"Черневецкий",
+            "translit"=>"Chernevetskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Черневецкий-район.png",
+        ],
+    
+        "Chechelnitsky"=>[
+            "alias"=>"Chechelnitsky",
+            "name"=>"Чечельницкий",
+            "translit"=>"Chechelnytskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Чечельницкий-район.png",
+        ],
+    
+        "Shargorod"=>[
+            "alias"=>"Shargorod",
+            "name"=>"Шаргородский",
+            "translit"=>"Sharhorodskyy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Шаргородский-район.png",
+        ],
+    
+        "Yampol"=>[
+            "alias"=>"Yampol",
+            "name"=>"Ямпольский",
+            "translit"=>"Yampolskiy",
+            "region"=>"Vinnytsia",
+            "available"=>"yes",
+            "map_img"=>"Ямпольский-район.png",
+        ],
+        
+    ],
+
+    "Odessa"=>[
+
+        "Ananyevsky"=>[
+            "alias"=>"Ananyevsky",
+            "name"=>"Ананьевский",
+            "translit"=>"Ananyevskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Ананьевский-район.png",
+        ],
+    
+        "Artsz"=>[
+            "alias"=>"Artsz",
+            "name"=>"Арцизский",
+            "translit"=>"Artsizskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Арцизский-район.png",
+        ],
+    
+        "Balt"=>[
+            "alias"=>"Balt",
+            "name"=>"Балтский",
+            "translit"=>"Baltskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Балтский-район.png",
+        ],
+    
+        "Belgorod-Dnestrovsky"=>[
+            "alias"=>"Belgorod-Dnestrovsky",
+            "name"=>"Белгород-Днестровский",
+            "translit"=>"Belgorod-Dnestrovskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Белгород-Днестровский-район.png",
+        ],
+    
+        "Belyaevsky"=>[
+            "alias"=>"Belyaevsky",
+            "name"=>"Беляевский",
+            "translit"=>"Belyayevskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Беляевский-район.png",
+        ],
+    
+        "Berezovsky"=>[
+            "alias"=>"Berezovsky",
+            "name"=>"Березовский",
+            "translit"=>"Berezovskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Березовский-район.png",
+        ],
+    
+        "Bolgradsky"=>[
+            "alias"=>"Bolgradsky",
+            "name"=>"Болградский",
+            "translit"=>"Bolgradskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Болградский-район.png",
+        ],
+    
+        "Velikomikhailovsky"=>[
+            "alias"=>"Velikomikhailovsky",
+            "name"=>"Великомихайловский",
+            "translit"=>"Velikomikhaylovskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Великомихайловский-район.png",
+        ],
+    
+        "Zakharevsky"=>[
+            "alias"=>"Zakharevsky",
+            "name"=>"Захарьевский",
+            "translit"=>"Zakharyevskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Захарьевский-район.png",
+        ],
+        
+        "Ivanovo"=>[
+            "alias"=>"Ivanovo",
+            "name"=>"Ивановский",
+            "translit"=>"Ivanovskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Ивановский-район.png",
+        ],
+    
+        "Izmail"=>[
+            "alias"=>"Izmail",
+            "name"=>"Измаильский",
+            "translit"=>"Izmailskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Измаильский-район.png",
+        ],
+    
+        "Kili"=>[
+            "alias"=>"Kili",
+            "name"=>"Килийский",
+            "translit"=>"Kiliyskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Килийский-район.png",
+        ],
+    
+        "Kodyma"=>[
+            "alias"=>"Kodyma",
+            "name"=>"Кодымский",
+            "translit"=>"Kodymskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Кодымский-район.png",
+        ],
+    
+        "Limansky"=>[
+            "alias"=>"Limansky",
+            "name"=>"Лиманский",
+            "translit"=>"Limanskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Лиманский-район.png",
+        ],
+    
+        "Lubashevsky"=>[
+            "alias"=>"Lubashevsky",
+            "name"=>"Любашёвский",
+            "translit"=>"Lyubashovskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Любашёвский-район.png",
+        ],
+    
+        "Nikolaevsky"=>[
+            "alias"=>"Nikolaevsky",
+            "name"=>"Николаевский",
+            "translit"=>"Nikolayevskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Николаевский-район.png",
+        ],
+    
+        "Ovidiopolsky"=>[
+            "alias"=>"Ovidiopolsky",
+            "name"=>"Овидиопольский",
+            "translit"=>"Ovidiopolskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Овидиопольский-район.png",
+        ],
+    
+        "Oknyansky"=>[
+            "alias"=>"Oknyansky",
+            "name"=>"Окнянский",
+            "translit"=>"Oknyanskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Окнянский-район.png",
+        ],
+    
+        "Podolsky"=>[
+            "alias"=>"Podolsky",
+            "name"=>"Подольский",
+            "translit"=>"Podolskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Подольский-район.png",
+        ],
+    
+        "Razdelniansky"=>[
+            "alias"=>"Razdelniansky",
+            "name"=>"Раздельнянский",
+            "translit"=>"Razdelnyanskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Раздельнянский-район.png",
+        ],
+    
+        "Reni"=>[
+            "alias"=>"Reni",
+            "name"=>"Ренийский",
+            "translit"=>"Reniyskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Ренийский-район.png",
+        ],
+    
+        "Savran"=>[
+            "alias"=>"Savran",
+            "name"=>"Савранский",
+            "translit"=>"Savranskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Савранский-район.png",
+        ],
+    
+        "Sarat"=>[
+            "alias"=>"Sarat",
+            "name"=>"Саратский",
+            "translit"=>"Saratskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Саратский-район.png",
+        ],
+    
+        "Tarutinsky"=>[
+            "alias"=>"Tarutinsky",
+            "name"=>"Тарутинский",
+            "translit"=>"Tarutynskyy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Тарутинский-район.png",
+        ],
+    
+        "Tatarbunar"=>[
+            "alias"=>"Tatarbunar",
+            "name"=>"Татарбунарский",
+            "translit"=>"Tatarbunarskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Татарбунарский-район.png",
+        ],
+    
+        "Shiryaevsky"=>[
+            "alias"=>"Shiryaevsky",
+            "name"=>"Ширяевский",
+            "translit"=>"Shiryayevskiy",
+            "region"=>"Odessa",
+            "available"=>"yes",
+            "map_img"=>"Ширяевский-район.png",
+        ],
+
+    ],
+
+];
---
+++ config/dict/sellertypes.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /**
+     * Типы продавцов.
+     */
+
+    "private"=>[
+        "alias"=>"private",
+        "name"=>"частное",
+        "translit"=>"chastnoye",
+    ],
+
+    "farming"=>[
+        "alias"=>"farming",
+        "name"=>"фермерское хозяйство",
+        "translit"=>"fermerskoye_khozyaystvo",
+    ],
+
+    "company"=>[
+        "alias"=>"company",
+        "name"=>"компания",
+        "translit"=>"kompaniya",
+    ],
+
+];
